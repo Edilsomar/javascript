05e4cf8df39b647cbc7027783a40fa0c1056307a
Edilsomar Oliveira <oliveiraedilsomar@gmail.com>
2026-02-28 12:52:41 -0300

    ...

--- condicoes aninhadas/JavaScript.js
+++
@@ -1,9 +0,0 @@
-var idade = 68
-console.log(`You are ${idade} years old!`)
-if (idade < 16){
-    console.log("Don't vote")
-} else  if( idade < 18 || idade >=  67){
-    console.log('Optional vote')
-} else {
-    console.log("Mandatory voting")
-} 
